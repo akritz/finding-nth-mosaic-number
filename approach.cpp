080823603a57372a419a35cbab82a5d023891892
akritz <58820591+akritz@users.noreply.github.com>
2019-12-12 22:02:29 +0530

    code approach

---
+++ approach.cpp
@@ -0,0 +1,2 @@
+We have to find all the prime factors and also the powers of the factors in the number by dividing the number by the factor until the factor divides the number 
+The Nth Mosaic number will then be the product of the found prime factors and their powers.
